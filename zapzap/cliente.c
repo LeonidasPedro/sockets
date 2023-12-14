c0af94045a560fa5559aa46154f8b2dda0e4aae6
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2023-12-13 22:45:21 -0300

    dasd

--- zapzap/cliente.c
+++ zapzap/cliente.c
@@ -7,42 +7,49 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void error(const char *msg) {
+void error(const char *msg)
+{
   perror(msg);
   exit(0);
 }
 
-void handle_received_message(int sender, const char *message) {
-    if (strncmp(message, "global:", 7) == 0) {
-        // Mensagem global
-        printf("Global: %s", message + 7); // Exclui o prefixo "global:"
-        // Broadcast para outros clientes se necessário
-    } else if (strncmp(message, "private:", 8) == 0) {
-        // Mensagem privada
-        int recipient_index;
-        char private_message[256];
-        if (sscanf(message, "private:%d:%s", &recipient_index, private_message) == 2) {
-            printf("Mensagem privada de Cliente %d para Cliente %d: %s\n", sender, recipient_index, private_message);
-            // Envie a mensagem privada para o cliente de índice recipient_index, se necessário
-        } else {
-            printf("Erro ao processar mensagem privada.\n");
-        }
-    } else {
-        // Mensagem desconhecida
-        printf("Mensagem desconhecida: %s", message);
+void handle_received_message(int sender, const char *message)
+{
+  if (strncmp(message, "global:", 7) == 0)
+  {
+    printf("Global: %s", message + 7);
+  }
+  else if (strncmp(message, "private:", 8) == 0)
+  {
+    int recipient_index;
+    char private_message[256];
+    if (sscanf(message, "private:%d:%s", &recipient_index, private_message) == 2)
+    {
+      printf("Mensagem privada de Cliente %d para Cliente %d: %s\n", sender, recipient_index, private_message + 2);
     }
+    else
+    {
+      printf("Erro ao processar mensagem privada.\n");
+    }
+  }
+  else
+  {
+    printf("Mensagem desconhecida: %s", message);
+  }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   int sockfd, portno, n;
   struct sockaddr_in serv_addr;
   struct hostent *server;
 
   char buffer[256];
-  fd_set readfds; // Conjunto de descritores de arquivo para select
-  struct timeval tv; // Tempo limite para select
+  fd_set readfds;
+  struct timeval tv;
 
-  if (argc < 3) {
+  if (argc < 3)
+  {
     fprintf(stderr, "usage %s hostname port\n", argv[0]);
     exit(0);
   }
@@ -56,7 +63,8 @@ int main(int argc, char *argv[]) {
 
   server = gethostbyname(argv[1]);
 
-  if (server == NULL) {
+  if (server == NULL)
+  {
     fprintf(stderr, "ERROR, no such host\n");
     exit(0);
   }
@@ -71,9 +79,8 @@ int main(int argc, char *argv[]) {
   if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
 
-  
-
- while (1) {
+  while (1)
+  {
     FD_ZERO(&readfds);
     FD_SET(STDIN_FILENO, &readfds);
     FD_SET(sockfd, &readfds);
@@ -81,32 +88,33 @@ int main(int argc, char *argv[]) {
     tv.tv_sec = 0;
     tv.tv_usec = 0;
 
-    if (select(sockfd + 1, &readfds, NULL, NULL, &tv) > 0) {
-        if (FD_ISSET(STDIN_FILENO, &readfds)) {
-            // Leitura da entrada padrão e envio ao servidor
-            bzero(buffer, 256);
-            fgets(buffer, 255, stdin);
-            n = write(sockfd, buffer, strlen(buffer));
+    if (select(sockfd + 1, &readfds, NULL, NULL, &tv) > 0)
+    {
+      if (FD_ISSET(STDIN_FILENO, &readfds))
+      {
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
 
-            if (n < 0)
-                error("ERROR writing to socket");
-        }
+        if (n < 0)
+          error("ERROR writing to socket");
+      }
 
-        if (FD_ISSET(sockfd, &readfds)) {
-            // Leitura da resposta do servidor
-            bzero(buffer, 256);
-            n = read(sockfd, buffer, 255);
+      if (FD_ISSET(sockfd, &readfds))
+      {
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
 
-            if (n < 0)
-                error("ERROR reading from socket");
+        if (n < 0)
+          error("ERROR reading from socket");
 
-            handle_received_message(sockfd, buffer);
+        handle_received_message(sockfd, buffer);
 
-            int i = strncmp("Bye", buffer, 3);
+        int i = strncmp("Bye", buffer, 3);
 
-            if (i == 0)
-                break;
-        }
+        if (i == 0)
+          break;
+      }
     }
   }
 
--- zapzap/servidor.c
+++ zapzap/servidor.c
@@ -10,66 +10,82 @@
 
 int client_sockets[MAX_CLIENTS];
 
-void error(const char *msg) {
+void error(const char *msg)
+{
     perror(msg);
     exit(1);
 }
 
-void send_to_user(int sender, int recipient, const char *message) {
-    char formatted_message[255 + 20];  // Adicione espaço suficiente para o prefixo e destinatário
+void send_to_user(int sender, int recipient, const char *message)
+{
+    char formatted_message[255 + 20];
     snprintf(formatted_message, sizeof(formatted_message), "private:%d:%s", recipient, message);
 
-    if (client_sockets[recipient] > 0) {
+    if (client_sockets[recipient] > 0)
+    {
         int n = write(client_sockets[recipient], formatted_message, strlen(formatted_message));
-        if (n < 0) {
+        if (n < 0)
+        {
             error("Erro ao escrever no socket");
         }
     }
 }
 
-void broadcast(int sender, const char *message) {
-    char formatted_message[255 + 10];  // Adicione espaço suficiente para o prefixo
+void broadcast(int sender, const char *message)
+{
+    char formatted_message[255 + 10];
     snprintf(formatted_message, sizeof(formatted_message), "global:%s", message);
 
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (i != sender && client_sockets[i] > 0) {
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        if (i != sender && client_sockets[i] > 0)
+        {
             int n = write(client_sockets[i], formatted_message, strlen(formatted_message));
-            if (n < 0) {
+            if (n < 0)
+            {
                 error("Erro ao escrever no socket");
             }
         }
     }
 }
 
-void *handle_client(void *arg) {
+void *handle_client(void *arg)
+{
     int client_socket = *((int *)arg);
     char buffer[256];
     int n;
 
-    while (1) {
+    while (1)
+    {
         bzero(buffer, 256);
         n = read(client_socket, buffer, 255);
-        if (n < 0) {
+        if (n < 0)
+        {
             error("Erro ao ler do socket");
         }
 
-        if (n == 0) {
-            // Cliente desconectado
+        if (n == 0)
+        {
+    
             printf("Cliente %d desconectado.\n", client_socket);
             break;
         }
 
         printf("Cliente %d: %s\n", client_socket, buffer);
 
-        // Verifica se a mensagem é privada e tem um destinatário
-        if (strncmp(buffer, "private:", 8) == 0) {
+        
+        if (strncmp(buffer, "private:", 8) == 0)
+        {
             int recipient;
-            if (sscanf(buffer + 8, "%d:", &recipient) == 1) {
-                // Extrai o destinatário da mensagem privada
+            if (sscanf(buffer + 8, "%d:", &recipient) == 1)
+            {
+                
                 send_to_user(client_socket, recipient, buffer + 8);
             }
-        } else {
-            // A mensagem não é privada, então é uma mensagem global
+        }
+        else
+        {
+           
             broadcast(client_socket, buffer);
         }
     }
@@ -78,8 +94,10 @@ void *handle_client(void *arg) {
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         fprintf(stderr, "Porta nao fornecida. Encerrando...\n");
         exit(1);
     }
@@ -88,11 +106,12 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in serv_addr, cli_addr;
     socklen_t clilen;
 
- // Array para armazenar os descritores de socket dos clientes
+    
     int num_clients = 0;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("Erro ao abrir o socket");
+    if (sockfd < 0)
+        error("Erro ao abrir o socket");
 
     bzero((char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
@@ -104,40 +123,45 @@ int main(int argc, char *argv[]) {
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("Erro ao fazer o bind");
 
-    listen(sockfd, MAX_CLIENTS); // Defina o número máximo de conexões para 8
+    listen(sockfd, MAX_CLIENTS); 
 
     printf("Aguardando conexões...\n");
 
-    while (1) {
+    while (1)
+    {
         clilen = sizeof(cli_addr);
 
         newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-        if (newsockfd < 0) {
+        if (newsockfd < 0)
+        {
             error("Erro ao aceitar a conexao");
         }
 
         printf("Novo cliente conectado.\n");
 
-        if (num_clients < MAX_CLIENTS) {
+        if (num_clients < MAX_CLIENTS)
+        {
             client_sockets[num_clients] = newsockfd;
 
-            // Inicie uma nova thread para lidar com o cliente
+            
             pthread_t client_thread;
-            if (pthread_create(&client_thread, NULL, handle_client, (void *)&client_sockets[num_clients]) != 0) {
+            if (pthread_create(&client_thread, NULL, handle_client, (void *)&client_sockets[num_clients]) != 0)
+            {
                 fprintf(stderr, "Erro ao criar thread para o cliente.\n");
                 close(newsockfd);
-            } else {
+            }
+            else
+            {
                 num_clients++;
             }
-        } else {
+        }
+        else
+        {
             printf("Limite máximo de clientes atingido. Novas conexões serão aceitas, mas não adicionadas ao array.\n");
-            // Note que a conexão ainda é aceita, mas não adicionada ao array
             close(newsockfd);
         }
-
     }
 
-    // Não alcançará este ponto, mas seria necessário fechar o socket principal
     close(sockfd);
 
     return 0;
